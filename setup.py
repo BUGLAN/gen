034517d7fa57d0d351d6a6864d73d6bff08a1b07
buglan <1831353087@qq.com>
2019-07-27 17:06:55 +0800

    update

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 from setuptools import setup, find_packages
 
+
+def long_long_description():
+    with open('README.md', 'r') as f:
+        desc = f.read()
+    return desc
+
+
 setup(
     name='simple-gen',
-    version='2019.5.12',
+    version='0.0.0',
     description='gen files',
+    long_description=long_long_description(),
     author='buglan',
     author_email='1831353087@qq.com',
     packages=find_packages(),
-    scripts=['gen.py'],
-    entry_points={
-        'console_scripts': [
-            'gen = gen:main',
-        ]
-    },
+    install_requires=['click'],
+    entry_points='''
+        [console_scripts]
+        hello=gen:hello
+    ''',
     setup_requires=[
         'pytest-runner'
     ],
